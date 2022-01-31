5589c2eba676f8a3f08d60d6690af496aca0f5c5
kraut <kraut>
2022-01-31 16:45:19 +0100

    Revert "14.0"
    This reverts commit 2df0c882d0159ba925b4a865331e75505e18936f.

--- models/attachments.py
+++
@@ -1,6 +0,0 @@
-from odoo import _, api, fields, models, SUPERUSER_ID
-from odoo.exceptions import UserError, RedirectWarning, ValidationError
-class Attachment(models.Model):
-    _inherit = 'ir.attachment'
-
-    
